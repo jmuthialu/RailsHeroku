99fbf170d1aef7dd05a5df72b5790fa3bda69780
jmuthialu <jmuthialu@yahoo.com>
2015-04-18 19:14:45 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appheroku1::Application.config.secret_token = '1463cc46e4d571712fb8d33cb33d631b58cda0850493b852b3b065a894a81124bb5f8e9e3cc261bb9b33cf7c6f3e7009e99c43e34d208016ce8e8d1e2b890a3f'
